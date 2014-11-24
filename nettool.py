64455179ee914c66fa664894663d0aca1430f47d
ubajze <uros@bajzelj.com>
2014-11-24 23:39:21 +0100

    Finished

--- nettool.py
+++ nettool.py
@@ -36,9 +36,6 @@ no_path_provided_msg = 'Path is not provided.'
 config_syntax_error_msg = 'There is a syntax error in the config file.'
 
 
-# za shranjevanje passworda uporabi keyring
-#naredis userja, ki edini poganja skripto s kompleksnim passwordom in das no logon
-#pod tem userjem uporabis keyring
 
 class Credentials:
 
@@ -137,16 +134,10 @@ class IosServices:
 
 
     def execute_multiple_commands(self,command_list):
-        #connection = HostConnection(self.host)
-        #connection.login_account(self.username,self.password)
-        #if self.enable_password:
-        #    connection.enable_account(self.enable_username,self.enable_password)
-        #connection.connect_to_device_ios()
         response_list = []
         for command in command_list:
             response = self.connection.execute_command(command)
             response_list.append(response)
-        #connection.disconnect_from_device()
         return response_list
 
     def save_string_to_file_system(self,string,path):
@@ -177,8 +168,6 @@ class IosServices:
         response_list = self.execute_multiple_commands(command_list)
         return response_list[0]
 
-    def get_vlans(self):
-        pass
 
 
 if __name__ == "__main__":
@@ -191,6 +180,7 @@ if __name__ == "__main__":
         'password': '',
         'enable_username': '',
         'enable_password': '',
+        'keyring': False,
         'type': device_types[0],
         'archive_location': '.',
         'save_running_config': False,
@@ -198,22 +188,18 @@ if __name__ == "__main__":
         'running_config_prefix': 'run_&h_&d',
         'get_startup_config': False,
         'startup_config_prefix': 'start_&h_&d',
-        'get_vlan_list': False,
-        'vlan_list_prefix': 'vlan_$h_$d',
+        'custom_cmd': None,
     }
     
 
     def parse_config(config_class):
 
-        #try:
         default_config = {}
         for key in config_parameters.keys():
             if config_class.has_option('default',key):
                 default_config[key] = config_class.get('default', key)
             else:
                 default_config[key] = config_parameters[key]
-        #except:
-         #   raise ConfigSyntaxError(config_syntax_error_msg)
 
 
         host_list = []
@@ -264,17 +250,33 @@ if __name__ == "__main__":
             file_path = path + "/" + filename
             return file_path
         else:
-            print "The %s does not exist." %host_data['archive_location']
+            error_msg = "The %s does not exist." %host_data['archive_location']
+            print_log(error_msg)
             return False
 
+    def get_custom_commands(custom_cmd_var):
+        try:
+            custom_commands_list = custom_cmd_var.split(':')
+            command = []
+            prefix = []
+            for element in custom_commands_list:
+                command.append(element[1:-1].split(',')[0])
+                prefix.append(element[1:-1].split(',')[1])
+            return command,prefix
+        except:
+            error_msg = "Parsing custom_cmd variable failed."
+            print_log(error_msg)
 
+    def print_log(msg,host = ''):
+        current_date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
+        log_msg = current_date + ' ' + host + ' ' + msg
+        print log_msg
         
 
     parser = argparse.ArgumentParser(prog='./nettool',description='Device management tool.')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-config', metavar='config', help='Provide path to config file.')
     group.add_argument('-host', metavar='host', help='The IP or hostname of the device.')
-    group.add_argument('-cred', metavar='username', help='Add username and password to keyring.')
     parser.add_argument('-user', metavar='username', help='The username for the device.')
     parser.add_argument('-pass', metavar='password', help='The password for the device.')
     parser.add_argument('-euser', metavar='username', help='The enable username for the device. (default: %s)' %config_parameters['enable_username'])
@@ -286,22 +288,40 @@ if __name__ == "__main__":
     parser.add_argument('-run_file', metavar='filename', help='The name of the file. (default: %s)' %config_parameters['running_config_prefix'])
     parser.add_argument('-start', action = 'store_true', help='Save the startup config. (default: %s)' %config_parameters['get_startup_config'])
     parser.add_argument('-start_file', metavar='filename', help='The name of the file. (default: %s)' %config_parameters['startup_config_prefix'])
-    parser.add_argument('-vlan', action = 'store_true', help='Save the running config. (default: %s)' %config_parameters['get_vlan_list'])
-    parser.add_argument('-vlan_file', metavar='filename', help='The name of the file. (default: %s)' %config_parameters['vlan_list_prefix'])
+    parser.add_argument('-keyring', action = 'store_true' , help='Use keyring to get passwords. (default: %s)' %config_parameters['keyring'])
+    parser.add_argument('-cmd', metavar='command_list' , help='Execute custom commands. (default: %s)' %config_parameters['custom_cmd'])
 
 
     args = vars(parser.parse_args())
 
     if not args['config']:
         config_parameters['hostname'] = args['host']
-        if not args['user']:
+        if args['user']:
+            config_parameters['username'] = args['user']
+        else:
             config_parameters['username'] = raw_input('Username: ')
-        if not args['pass']:
-            config_parameters['password'] = getpass.getpass()
+        if args['pass']:
+            config_parameters['password'] = args['pass']
+        else:
+            if args['keyring']:
+                try:
+                    config_parameters['password'] = Credentials.get_password(config_parameters['username'])
+                except UserDoesNotExist as err:
+                    print_log(str(err), config_parameters['hostname'])
+                    #print str(err) + ' Host: %s.' %config_parameters['hostname']
+            else:
+                config_parameters['password'] = getpass.getpass()
         if args['euser']:
             config_parameters['enable_username'] = args['euser']
-        if not args['epass']:
-            config_parameters['enable_password'] = getpass.getpass('Enable password: ')
+        else:
+            config_parameters['enable_username'] = raw_input('Enable username: ')
+        if args['epass']:
+            config_parameters['enable_password'] = args['epass']
+        else:
+            if args['keyring']:
+                config_parameters['enable_password'] = Credentials.get_password(config_parameters['enable_username'])
+            else:
+                config_parameters['enable_password'] = getpass.getpass('Enable password: ')
         if args['type']:
             config_parameters['type'] = args['type']
         if args['path']:
@@ -316,10 +336,8 @@ if __name__ == "__main__":
             config_parameters['get_startup_config'] = args['start']
         if args['start_file']:
             config_parameters['startup_config_prefix'] = args['start_file']
-        if args['vlan']:
-            config_parameters['get_vlan_list'] = args['vlan']
-        if args['vlan_file']:
-            config_parameters['vlan_list_prefix'] = args['vlan_file']
+        if args['cmd']:
+            config_parameters['custom_cmd'] = args['cmd']
         hosts = [config_parameters]
 
 
@@ -329,7 +347,8 @@ if __name__ == "__main__":
         exit_program = False
         for key in args.keys():
             if args[key]:
-                print "Argument %s is not allowed in config submode." %key
+                error_msg = "Argument %s is not allowed in config submode." %key
+                print_log(error_msg)
                 exit_program = True
         if exit_program:
             sys.exit(0)
@@ -338,44 +357,75 @@ if __name__ == "__main__":
         config.read(config_location)
         hosts = parse_config(config)
 
-
+        for host in hosts:
+            if host['keyring']:
+                try:
+                    host['password'] = Credentials.get_password(host['username'])
+                    host['enable_password'] = Credentials.get_password(host['enable_username'])
+                except UserDoesNotExist as err:
+                    print_log(str(err),host['hostname'])
 
     for host in hosts:
         host_service = IosServices(host['hostname'],host['username'],host['password'],host['enable_username'],host['enable_password'])
         try:
             host_service.connect_to_device()
         except ConnectionError as err:
-            print str(err)
+            print_log(str(err),host['hostname'])
             continue
         except AuthenticationError as err:
-            print str(err) + ' Operation: %s, Host: %s' %('Save running config', host['hostname'])
+            print_log(str(err),host['hostname'])
             continue
         if host['save_running_config']:
             if config_parameters['type'] == 'ios':
                 try:
                     host_service.save_configuration()
-                    print "The running-config on the host %s was successfully saved to startup-config." %host['hostname']
+                    error_msg = "The running-config on the host %s was successfully saved to startup-config." %host['hostname']
+                    print_log(error_msg,host['hostname'])
                 except CommandError as err:
-                    print str(err) + ' Operation: %s, Host: %s' %('Save running config', host['hostname'])
+                    error_msg = str(err) + ' Operation: %s.' %'Save running config'
+                    print_log(error_msg,host['hostname'])
         if host['get_running_config']:
             file_path = get_file_path(host['archive_location'],get_filename(host['running_config_prefix'],host['hostname']))
             if file_path:
                 if config_parameters['type'] == 'ios':
                     try:
                         host_service.get_running_config(save_to_file_system=True, path = file_path)
-                        print "Running-config for host %s was successfully saved to the %s." %(host['hostname'],file_path)
+                        error_msg = "Running-config for host %s was successfully saved to the %s." %(host['hostname'],file_path)
+                        print_log(error_msg,host['hostname'])
                     except CommandError as err:
-                        print str(err) + ' Operation: %s, Host: %s' %('Get running config', host['hostname'])
+                        error_msg = str(err) + ' Operation: %s.' %'Get running config'
+                        print_log(error_msg,host['hostname'])
         if host['get_startup_config']:
             file_path = get_file_path(host['archive_location'],get_filename(host['startup_config_prefix'],host['hostname']))
             if file_path:
                 if config_parameters['type'] == 'ios':
-                    running_config = IosServices(host['hostname'],host['username'],host['password'],host['enable_username'],host['enable_password'])
                     try:
                         host_service.get_startup_config(save_to_file_system=True, path = file_path)
-                        print "Startup-config for host %s was successfully saved to the %s." %(host['hostname'],file_path)
+                        error_msg = "Startup-config for host %s was successfully saved to the %s." %(host['hostname'],file_path)
+                        print_log(error_msg,host['hostname'])
                     except CommandError as err:
-                        print str(err) + ' Operation: %s, Host: %s' %('Get startup config', host['hostname'])
+                        error_msg = str(err) + ' Operation: %s.' %'Get startup config'
+                        print_log(error_msg,host['hostname'])
+        if host['custom_cmd']:
+            commands,prefixes = get_custom_commands(host['custom_cmd'])
+            try:
+                command_outputs = host_service.execute_multiple_commands(commands)
+            except CommandError as err:
+                error_msg = str(err) + ' Operation: %s.' %'Custom command'
+                print_log(error_msg,host['hostname']) 
+            try:
+                for output in command_outputs:
+                    current_postion = command_outputs.index(output)
+                    file_path = get_file_path(host['archive_location'],get_filename(prefixes[current_postion]))
+                    host_service.save_string_to_file_system(output, file_path)
+                    error_msg = "Output of the command '%s' was successfully saved to the %s." %(commands[current_postion],file_path)
+                    print_log(error_msg,host['hostname'])
+            except:
+                error_msg = "Saving outputs of the custom commands to file system failed."
+                print_log(error_msg,host['hostname'])
+
+    
+
         host_service.disconnect_from_device()
 
 
